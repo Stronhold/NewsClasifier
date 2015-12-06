4bc3464201b7ad8d08ed359b2a06fdab692d491e
stronhold <sergioausin1993@gmail.com>
2015-12-06 18:13:16 +0100

    Added some things

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.esotericsoftware.kryo.Kryo;
+import edu.upc.freeling.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -7,8 +9,12 @@ import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.text.Normalizer;
 import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * Created by Sergio on 02/12/2015.
@@ -16,17 +22,22 @@ import java.util.*;
 public class Main {
 
     public static void main(String [] args){
-        //Path donde estarán las categorías
+
+        //Path donde estaran las categorias
         String pathCategories = "src/main/resources/categories/";
-        //Configuración básica de la aplicación
+
+        //Configuracion basica de la aplicacion
         SparkConf sparkConf = new SparkConf().setAppName("NaiveBayes").setMaster("local[*]");
-        //Creación del contexto
+
+        //Creacion del contexto
         JavaSparkContext jsc = new JavaSparkContext(sparkConf);
-        //Cogemos el fichero en el que se encuentran las categorías
+
+        //Cogemos el fichero en el que se encuentran las categorias
         File f = new File(pathCategories);
-        //Listamos las categorías
+        //Listamos las categorias
         String [] categories = f.list();
-        //Mostramos las categorías :)
+        HashMap<String, String> dictionary = loadDictionary();
+        //Mostramos las categorias :)
         for(String c : categories){
             System.out.println(c);
             try {
@@ -56,9 +67,30 @@ public class Main {
                         s = s.replace("}", "");
                         s = s.replace("-", "");
                         s = s.replace("_", "");
-                        return new Tuple2<String, Integer>(s, 1);
+                        s = Normalizer.normalize(s, Normalizer.Form.NFD);
+                        Pattern pattern = Pattern.compile("\\P{ASCII}+");
+                        s = pattern.matcher(s).replaceAll("");
+                        if (!dictionary.containsKey(s) && !isNumeric(s)) {
+                            return new Tuple2<String, Integer>(s, 1);
+                        } else {
+                            return new Tuple2<String, Integer>("", 1);
+                        }
                     }
-                }).reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+                    private boolean isNumeric(String s) {
+                        try
+                        {
+                            double d = Double.parseDouble(s);
+                        }
+                        catch(NumberFormatException nfe)
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
+
+                })
+                .reduceByKey(new Function2<Integer, Integer, Integer>() {
                     @Override
                     public Integer call(Integer integer, Integer integer2) throws Exception {
                         return integer + integer2;
@@ -66,7 +98,13 @@ public class Main {
                 });
                 List<Tuple2<String, Integer>> total = wordCount.collect();
                 Collections.sort(total, (o1, o2) -> (o1._2.compareTo(o2._2))*-1);
+                List<Tuple2<String, Integer>> elementsRemoved = new ArrayList<>();
                 for(Tuple2<String, Integer> t : total){
+                    if(!t._1.equals("")){
+                        elementsRemoved.add(t);
+                    }
+                }
+                for(Tuple2<String, Integer> t : elementsRemoved){
                     System.out.println(t._1 + " " + t._2);
                 }
             }catch (Exception e){
@@ -74,5 +112,19 @@ public class Main {
             }
         }
         jsc.stop();
+    }
+
+    private static HashMap<String, String> loadDictionary() {
+        HashMap<String, String> map = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/StopWords.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                map.put(line, line);
+            }
+        }catch (Exception e){
+
+        }
+
+        return map;
     }
 }
